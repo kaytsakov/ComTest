ba0fcc8442bcfecea1a93d70ed33d6d63306b987
kaytsakov <slavikkaytsakov@boo-vision.com>
2024-08-26 11:10:51 +0400

    first commit

---
+++ ComPortTest/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿
+namespace ComPortTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            if (serialPort1.IsOpen) serialPort1.Close();
+            if (serialPort2.IsOpen) serialPort2.Close();
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_Read);
+            // 
+            // serialPort2
+            // 
+            this.serialPort2.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort2_Read);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(896, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "End Port";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            this.serialPort1,
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "57600",
+            "76800",
+            "115200",
+            "230400",
+            "250000"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 62);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(849, 32);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 4;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            this.serialPort2,
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "57600",
+            "76800",
+            "115200",
+            "230400",
+            "250000"});
+            this.comboBox4.Location = new System.Drawing.Point(849, 62);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 5;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(768, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(768, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Open";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(341, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Count";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(447, 128);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "START";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(301, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Average:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(299, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Minimum:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(296, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Maximum:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(372, 196);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(372, 224);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(372, 252);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(478, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(220, 38);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Microseconds";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(341, 44);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(181, 22);
+            this.textBox5.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(341, 19);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 17);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Transmit data";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(342, 78);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(201, 21);
+            this.checkBox1.TabIndex = 23;
+            this.checkBox1.Text = "One-way data transmission";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 453);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ ComPortTest/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComPortTest
+{
+    public partial class Form1 : Form
+    {
+        long picoSecondsMax = 0;
+        long picoSecondsAvr = 0;
+        long picoSecondsMin = 0;
+
+        long CountOfCykles = 0;
+        long setCountData = 0;
+
+        string TxData="DATA";
+
+        DateTime picoSecondsFixDataTx;
+        DateTime picoSecondsFixDataRx;
+
+        long Ticks1 = 0;
+        long Ticks2 = 0;
+        Stopwatch sw = new Stopwatch();
+        //sw.Start();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Enabled && !serialPort1.IsOpen) serialPort1.Open();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TxData = textBox5.Text;
+
+            if (serialPort1.IsOpen) button1.BackColor = Color.GreenYellow;
+            else button1.BackColor = Color.Red;
+
+            if (serialPort2.IsOpen) button4.BackColor = Color.GreenYellow;
+            else button4.BackColor = Color.Red;
+
+            if (serialPort1.IsOpen && serialPort2.IsOpen && textBox1.Text != "" && textBox5.Text != "") button5.Visible = true;
+            else button5.Visible = false;
+
+            if (checkBox1.Checked == true) checkBox1.Invoke(new Action(() => checkBox1.Text = "Round-trip data transmission"));
+            else checkBox1.Invoke(new Action(() => checkBox1.Text = "One-way data transmission"));
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                if (!comboBox1.Items.Contains(s))
+                {
+                    // Console.WriteLine(s);
+                    // serialPortNames[portNamesCount]=s;
+                    // portNamesCount++;
+                    comboBox1.Items.Add(s);
+                    comboBox3.Items.Add(s);
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.Write("Port 1: ");
+            Console.WriteLine(comboBox1.Text);
+            serialPort1.PortName = comboBox1.Text;
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.Write("Port 2: ");
+            Console.WriteLine(comboBox3.Text);
+            serialPort2.PortName = comboBox3.Text;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedIndex)
+            {
+                case 1:
+                    serialPort1.BaudRate = 2400;
+                    break;
+                case 2:
+                    serialPort1.BaudRate = 4800;
+                    break;
+                case 3:
+                    serialPort1.BaudRate = 9600;
+                    break;
+                case 4:
+                    serialPort1.BaudRate = 14400;
+                    break;
+                case 5:
+                    serialPort1.BaudRate = 19200;
+                    break;
+                case 6:
+                    serialPort1.BaudRate = 28800;
+                    break;
+                case 7:
+                    serialPort1.BaudRate = 38400;
+                    break;
+                case 8:
+                    serialPort1.BaudRate = 57600;
+                    break;
+                case 9:
+                    serialPort1.BaudRate = 76800;
+                    break;
+                case 10:
+                    serialPort1.BaudRate = 115200;
+                    break;
+                case 11:
+                    serialPort1.BaudRate = 230400;
+                    break;
+                case 12:
+                    serialPort1.BaudRate = 250000;
+                    break;
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox4.SelectedIndex)
+            {
+                case 1:
+                    serialPort2.BaudRate = 2400;
+                    break;
+                case 2:
+                    serialPort2.BaudRate = 4800;
+                    break;
+                case 3:
+                    serialPort2.BaudRate = 9600;
+                    break;
+                case 4:
+                    serialPort2.BaudRate = 14400;
+                    break;
+                case 5:
+                    serialPort2.BaudRate = 19200;
+                    break;
+                case 6:
+                    serialPort2.BaudRate = 28800;
+                    break;
+                case 7:
+                    serialPort2.BaudRate = 38400;
+                    break;
+                case 8:
+                    serialPort2.BaudRate = 57600;
+                    break;
+                case 9:
+                    serialPort2.BaudRate = 76800;
+                    break;
+                case 10:
+                    serialPort2.BaudRate = 115200;
+                    break;
+                case 11:
+                    serialPort2.BaudRate = 230400;
+                    break;
+                case 12:
+                    serialPort2.BaudRate = 250000;
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+            long T1, T2;
+            T1 = DateTime.Now.Ticks;
+            T2 = DateTime.Now.Ticks;
+            Console.WriteLine(T2 - T1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (serialPort2.IsOpen) serialPort2.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Enabled && !serialPort2.IsOpen) serialPort2.Open();
+        }
+
+        private void serialPort1_Read(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                long TimeOfRxTx = 0;
+                string ReadDarta;
+                ReadDarta = serialPort1.ReadLine();
+                serialPort1.DiscardOutBuffer();
+                serialPort1.DiscardInBuffer();
+                sw.Stop();
+                long microseconds = sw.ElapsedTicks / (Stopwatch.Frequency / (1000L * 1000L));
+
+                TimeOfRxTx = microseconds;
+                sw.Reset();
+                Console.Write("Round-trip: ");
+                Console.WriteLine(TimeOfRxTx);
+
+                if (ReadDarta == TxData)
+                {
+                    if (picoSecondsAvr == 0 && picoSecondsMax == 0 && picoSecondsMin == 0)
+                    {
+                        picoSecondsAvr = TimeOfRxTx;
+                        picoSecondsMax = TimeOfRxTx;
+                        picoSecondsMin = TimeOfRxTx;
+                    }
+                    else
+                    {
+                        if (TimeOfRxTx > picoSecondsMax) picoSecondsMax = TimeOfRxTx;
+                        if (TimeOfRxTx < picoSecondsMin && TimeOfRxTx != 0) picoSecondsMin = TimeOfRxTx;
+                        picoSecondsAvr += TimeOfRxTx;
+                    }
+                    CountOfCykles++;
+                    if (CountOfCykles < setCountData)
+                    {
+                        sw.Start();
+                        serialPort1.WriteLine(TxData);
+                    }
+                    else
+                    {
+                        picoSecondsAvr /= setCountData;
+                        textBox2.Invoke(new Action(() => textBox2.Text = picoSecondsAvr.ToString()));
+                        textBox3.Invoke(new Action(() => textBox3.Text = picoSecondsMin.ToString()));
+                        textBox4.Invoke(new Action(() => textBox4.Text = picoSecondsMax.ToString()));
+                    }
+                }
+            }
+        }
+
+        private void serialPort2_Read(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                long TimeOfRxTx = 0;
+                string ReadDarta;
+                ReadDarta = serialPort2.ReadLine();
+                serialPort2.DiscardOutBuffer();
+                serialPort2.DiscardInBuffer();
+                sw.Stop();
+                long microseconds = sw.ElapsedTicks / (Stopwatch.Frequency / (1000L * 1000L));
+
+                TimeOfRxTx = microseconds;
+                sw.Reset();
+                Console.Write("One-way: ");
+                Console.WriteLine(TimeOfRxTx);
+
+                if (ReadDarta == TxData)
+                {
+                    if (picoSecondsAvr == 0 && picoSecondsMax == 0 && picoSecondsMin == 0)
+                    {
+                        picoSecondsAvr = TimeOfRxTx;
+                        picoSecondsMax = TimeOfRxTx;
+                        picoSecondsMin = TimeOfRxTx;
+                    }
+                    else
+                    {
+                        if (TimeOfRxTx > picoSecondsMax) picoSecondsMax = TimeOfRxTx;
+                        if (TimeOfRxTx < picoSecondsMin && TimeOfRxTx != 0) picoSecondsMin = TimeOfRxTx;
+                        picoSecondsAvr += TimeOfRxTx;
+                    }
+                    CountOfCykles++;
+                    if (CountOfCykles < setCountData)
+                    {
+                        sw.Start();
+                        serialPort1.WriteLine(TxData);
+                    }
+                    else
+                    {
+                        picoSecondsAvr /= setCountData;
+                        textBox2.Invoke(new Action(() => textBox2.Text = picoSecondsAvr.ToString()));
+                        textBox3.Invoke(new Action(() => textBox3.Text = picoSecondsMin.ToString()));
+                        textBox4.Invoke(new Action(() => textBox4.Text = picoSecondsMax.ToString()));
+                    }
+                }
+            }
+            else
+            {
+                //long TimeOfRxTx = 0;
+                string ReadDarta;
+                ReadDarta = serialPort2.ReadLine();
+                serialPort2.DiscardOutBuffer();
+                serialPort2.DiscardInBuffer();
+                serialPort2.WriteLine(ReadDarta);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CountOfCykles = 0;
+            picoSecondsAvr = 0;
+            picoSecondsMax = 0;
+            picoSecondsMin = 0;
+            setCountData = Convert.ToInt32(textBox1.Text);
+            //Console.WriteLine(setCountData);
+            //picoSecondsFixDataTx = DateTime.Now;
+            sw.Start();
+            //Ticks1 = DateTime.Now.Ticks;
+            serialPort1.WriteLine(TxData);
+            //picoSecondsFixDataTx = DateTime.Now;
+        }
+    }
+}
